79e15ef4f41386bed6a2604fbdbfbe7b63491f43
Gilson Vieira <gilson.vieira@clickbus.com>
2018-09-09 22:50:26 -0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-#from distutils.core import setup
 from setuptools import setup
 
 setup(
-    name='file.organizer',
-    packages=['file.organizer'],
-    version='0.1',
-    description='A file organizer based on file suffixes and user chosen directories',
+    name='file_organizer',
+    py_modules=['organize'],
+    version='0.2.0',
+    description='A file organizer based on file suffixes and user chosen file extensions',
     author='Gilson de Paula',
     author_email='gilsonsvieira93@gmail.com',
     url='https://github.com/xlbruce/file-organizer',
     license='MIT',
-    download_url='https://github.com/xlbruce/file-organizer/archive/0.1.tar.gz',
     keywords=['file', 'organizer'],
-    classifiers=[]
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        organize=organize:main
+    ''',
 )
