fd7edcfaaa3a0a486f6c4da05468f2f198bea3e0
Gilson Vieira <gilson.vieira@clickbus.com>
2018-09-09 23:23:09 -0300

    Added README as long description

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
+with open('README.md', 'r') as f:
+        long_description = f.read()
+
 setup(
     name='file_organizer',
     py_modules=['organize'],
-    version='0.2.0',
+    version='0.2.1',
     description='A file organizer based on file suffixes and user chosen file extensions',
+    long_description=long_description,
     author='Gilson de Paula',
     author_email='gilsonsvieira93@gmail.com',
     url='https://github.com/xlbruce/file-organizer',
@@ -17,4 +21,9 @@ setup(
         [console_scripts]
         organize=organize:main
     ''',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: POSIX",
+
+    ]
 )
